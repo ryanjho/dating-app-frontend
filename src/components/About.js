de2d996debe5a3b3514827ed7aadcb199a99b1d7
Ryan Ho <ryanjho90@gmail.com>
2020-08-04 19:27:38 +0800

    Added About and FAQ pages

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-      <div>
-        <p>About Us</p>
+      <div className="container">
+        <h2>About Us</h2>
+        <p>Dating App is the latest and greatest dating application for individuals looking for true love.</p>
+        <p>This application allows you to create your own profile, specify your date preferences, filter and like profiles that you would like to chat with. If the other person also likes your profile, you would be able to chat with them in the app!</p>
+        <p>Dating App is proudly brought to you by RML Group (<a href="https://github.com/ryanjho">Ryan Ho</a>, <a href="https://github.com/linhkhanh">Linh Le</a> and <a href="https://github.com/mariusandrian">Marius Andrian</a>) the best software engineering team in Singapore and Southeast Asia.</p>
       </div>
     )
   }
--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 class FAQ extends Component {
   render() {
     return (
-      <div>
-        <p>FAQ</p>
+      <div className="container">
+        <h2>Frequently Asked Questions</h2>
+        <h4>Question 1</h4>
+        <p>Pariatur magna fugiat ex culpa velit est. Ad laboris consectetur aliqua aliquip aute id duis cupidatat aliqua labore. Elit voluptate ullamco cupidatat dolore eiusmod sint ut et fugiat et duis. Consequat consectetur occaecat veniam eiusmod occaecat velit adipisicing ullamco est veniam aute deserunt sint dolor.</p>
+        <h4>Question 2</h4>
+        <p>Pariatur magna fugiat ex culpa velit est. Ad laboris consectetur aliqua aliquip aute id duis cupidatat aliqua labore. Elit voluptate ullamco cupidatat dolore eiusmod sint ut et fugiat et duis. Consequat consectetur occaecat veniam eiusmod occaecat velit adipisicing ullamco est veniam aute deserunt sint dolor.</p>
+        <h4>Question 3</h4>
+        <p>Pariatur magna fugiat ex culpa velit est. Ad laboris consectetur aliqua aliquip aute id duis cupidatat aliqua labore. Elit voluptate ullamco cupidatat dolore eiusmod sint ut et fugiat et duis. Consequat consectetur occaecat veniam eiusmod occaecat velit adipisicing ullamco est veniam aute deserunt sint dolor.</p>
+        <h4>Question 4</h4>
+        <p>Pariatur magna fugiat ex culpa velit est. Ad laboris consectetur aliqua aliquip aute id duis cupidatat aliqua labore. Elit voluptate ullamco cupidatat dolore eiusmod sint ut et fugiat et duis. Consequat consectetur occaecat veniam eiusmod occaecat velit adipisicing ullamco est veniam aute deserunt sint dolor.</p>
+        <h4>Question 5</h4>
+        <p>Pariatur magna fugiat ex culpa velit est. Ad laboris consectetur aliqua aliquip aute id duis cupidatat aliqua labore. Elit voluptate ullamco cupidatat dolore eiusmod sint ut et fugiat et duis. Consequat consectetur occaecat veniam eiusmod occaecat velit adipisicing ullamco est veniam aute deserunt sint dolor.</p>
       </div>
     )
   }
