ae7a94fd6902cc2d38d3f41cce2f79449a374c43
linhle <linhle@Hieus-MacBook-Air.local>
2020-08-08 15:58:14 +0800

    design app

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { Redirect } from "react-router-dom";
 import { Carousel } from 'react-responsive-carousel';
-import { Link } from 'react-router-dom';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+import ChatIcon from '@material-ui/icons/Chat';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 class ListUser extends Component {
@@ -17,31 +19,6 @@ class ListUser extends Component {
 
                     <div>
                         <p className="text-center">{this.props.foundUsers} users found</p>
-                        <div className="sub-nav">
-                            {/* find near by users */}
-
-                            <Link to="/near">
-                            <Button variant="light" onClick={this.props.findNearByUser}><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M11 4a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                <path d="M7.5 4h1v9a.5.5 0 0 1-1 0V4z" />
-                                <path fill-rule="evenodd" d="M6.489 12.095a.5.5 0 0 1-.383.594c-.565.123-1.003.292-1.286.472-.302.192-.32.321-.32.339 0 .013.005.085.146.21.14.124.372.26.701.382.655.246 1.593.408 2.653.408s1.998-.162 2.653-.408c.329-.123.56-.258.701-.382.14-.125.146-.197.146-.21 0-.018-.018-.147-.32-.339-.283-.18-.721-.35-1.286-.472a.5.5 0 1 1 .212-.977c.63.137 1.193.34 1.61.606.4.253.784.645.784 1.182 0 .402-.219.724-.483.958-.264.235-.618.423-1.013.57-.793.298-1.855.472-3.004.472s-2.21-.174-3.004-.471c-.395-.148-.749-.336-1.013-.571-.264-.234-.483-.556-.483-.958 0-.537.384-.929.783-1.182.418-.266.98-.47 1.611-.606a.5.5 0 0 1 .595.383z" />
-                            </svg></Button>
-                            </Link>
-
-                  
-
-
-                            {/* filter users */}
-                            <Button variant="light">
-                                <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-card-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" d="M14.5 3h-13a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
-                                    <path fillRule="evenodd" d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5z" />
-                                    <circle cx="3.5" cy="5.5" r=".5" />
-                                    <circle cx="3.5" cy="8" r=".5" />
-                                    <circle cx="3.5" cy="10.5" r=".5" />
-                                </svg>
-                            </Button>
-                        </div>
                         {this.props.users ?
                             <div className="carousel-wrapper">
                                 <Carousel className="carousel-style" showThumbs={false}>
@@ -70,26 +47,18 @@ class ListUser extends Component {
                                                     <div className="sub-nav">
                                                         {/* ignore user */}
 
-                                                        <Button a-key={user._id} onClick={this.skipUser} variant="btn btn-light">
-                                                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                                <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z" />
-                                                                <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z" />
-                                                                </svg>
+                                                        <Button a-key={user._id} onClick={this.skipUser} variant="danger">
+                                                            <HighlightOffIcon />
                                                         </Button>
 
                                                         {/* message */}
-                                                        <Button variant="btn btn-light">
-                                                            <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-chat-text" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                                <path fillRule="evenodd" d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
-                                                                <path fillRule="evenodd" d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z" />
-                                                            </svg>
+                                                        <Button variant="warning">
+                                                            <ChatIcon />
                                                         </Button>
 
                                                         {/* Like user */}
-                                                        <Button a-key={user._id} onClick={this.props.likeUser} variant="btn btn-light">
-                                                            <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-suit-heart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                                <path fillRule="evenodd" d="M8 6.236l.894-1.789c.222-.443.607-1.08 1.152-1.595C10.582 2.345 11.224 2 12 2c1.676 0 3 1.326 3 2.92 0 1.211-.554 2.066-1.868 3.37-.337.334-.721.695-1.146 1.093C10.878 10.423 9.5 11.717 8 13.447c-1.5-1.73-2.878-3.024-3.986-4.064-.425-.398-.81-.76-1.146-1.093C1.554 6.986 1 6.131 1 4.92 1 3.326 2.324 2 4 2c.776 0 1.418.345 1.954.852.545.515.93 1.152 1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z" />
-                                                            </svg>
+                                                        <Button a-key={user._id} onClick={this.props.likeUser} variant="success">
+                                                            <FavoriteBorderIcon />
                                                         </Button>
                                                     </div>
 
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -13,53 +13,44 @@ export class SignUpForm extends Component {
         return (
             <React.Fragment>
                 <Form onSubmit={handleFormSubmit} className="signup-form">
-                        <h2 className="text-center h2">CREATE NEW ACCOUNT</h2>
+                    <h2 className="text-center h2">SIGN UP</h2>
+                    <div class="modal-body mx-3">
+                        <div class="md-form mb-5">
+                            <i class="fas fa-envelope prefix grey-text"></i>
+                            <input type="email" placeholder="Enter email" id="email" value={email} onChange={handleFormChange} required className="form-control validate" />
+                        </div>
 
-                        <Form.Group as={Row}>
-                            <Form.Label column sm={4}>Email</Form.Label>
-                            <Col sm={8}>
-                                <Form.Control type="email" placeholder="Enter email" id="email" value={email} onChange={handleFormChange} required />
-                            </Col>
-                        </Form.Group>
+                        <div class="md-form mb-4">
+                            <i class="fas fa-lock prefix grey-text"></i>
+                            <input type="password" placeholder="Password" id="password" value={password} onChange={handleFormChange} required className="form-control validate" />
 
-                        <Form.Group as={Row}>
-                            <Form.Label column sm={4}>Password</Form.Label>
-                            <Col sm={8}>
-                                <Form.Control type="password" placeholder="Password" id="password" value={password} onChange={handleFormChange} required />
-                            </Col>
-                        </Form.Group>
+                        </div>
 
-                        <Form.Group as={Row}>
-                            <Form.Label column sm={4}>UserName</Form.Label>
-                            <Col sm={8}>
-                                <Form.Control type="string" placeholder="Enter user name" id="userName" value={userName} onChange={handleFormChange} required />
-                            </Col>
-                        </Form.Group>
+                        <div class="md-form mb-5">
+                            <i class="fas fa-user prefix grey-text"></i>
+                            <input type="string" placeholder="Enter user name" id="userName" value={userName} onChange={handleFormChange} required className="form-control validate" />
+                        </div>
 
-                        <Form.Group as={Row}>
-                            <Form.Label column sm={4}>Age</Form.Label>
-                            <Col sm={8}>
-                                <Form.Control type="number" id="age" value={age} onChange={handleFormChange} min='18' required />
-                            </Col>
-                        </Form.Group>
+                        <div class="md-form mb-5">
+                            <i class="fas fa-child prefix grey-text"></i>
+                            <input type="number" id="age" value={age} onChange={handleFormChange} min='18' required className="form-control validate" />
+                        </div>
 
-                        <Form.Group as={Row}>
-                            <Form.Label column sm={4}>Location</Form.Label>
-                            <Col sm={8}>
-                                <input type="text" name="name" list="countries" value={location} placeholder="Enter your location" id="location" onChange={handleFormChange} />
-                                <datalist id="countries">
-                                    {countries ?
-                                        countries.map((country, key) => {
-                                            return (
-                                                <option key={key}>{country.name}</option>
-                                            )
-                                        }) : ''}
-                                </datalist>
-                            </Col>
-                        </Form.Group>
+                        <div class="md-form mb-5">
+                            <i class="fas fa-globe-americas prefix grey-text"></i>
+                            <input type="text" name="name" list="countries" value={location} placeholder="Enter your location" id="location" onChange={handleFormChange} className="form-control validate" />
+                            <datalist id="countries">
+                                {countries ?
+                                    countries.map((country, key) => {
+                                        return (
+                                            <option key={key}>{country.name}</option>
+                                        )
+                                    }) : ''}
+                            </datalist>
 
+                        </div>
                         <Form.Group as={Row}>
-                            <Form.Label column sm={4}>Gender</Form.Label>
+                            <Form.Label column sm={2}><i class="fas fa-transgender-alt prefix grey-text"></i></Form.Label>
                             <Col>
                                 <Form.Check inline label="Female" id="female" checked={female} onChange={toggleGender} />
                                 <Form.Check inline label="Male" id="male" checked={male} onChange={toggleGender} />
@@ -67,7 +58,7 @@ export class SignUpForm extends Component {
                         </Form.Group>
 
                         <Form.Group as={Row} encType="multipart/form-data">
-                            <Form.Label column sm={4}>Avatar</Form.Label>
+                            <Form.Label column sm={2}><i class="far fa-image prefix grey-text"></i></Form.Label>
                             <Col sm={8}>
                                 <Form.Control type="file" onChange={handleFormChange} id="image" required />
                             </Col>
@@ -92,13 +83,15 @@ export class SignUpForm extends Component {
                                 </Form.Group>
                             </Col>
                         </Form.Group>
+                    </div>
 
-                        <Form.Group className="text-center">
-                            <Button variant="primary" type="submit"> Submit</Button>
-                        </Form.Group>
 
-                    </Form>
-            </React.Fragment>
+                    <Form.Group className="text-center">
+                        <Button variant="primary" type="submit"> Submit</Button>
+                    </Form.Group>
+
+                </Form>
+            </React.Fragment >
         )
     }
 }
